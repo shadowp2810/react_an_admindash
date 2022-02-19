fcd535da5e70f2d5279aa9663c0678fe4572f73f
shadowp2810 <66818159+shadowp2810@users.noreply.github.com>
2022-02-19 21:50:47 +0530

    Build TopNavBar component.

---
+++ src/components/topnavbar/TopNavBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./TopNavBar.css";
+import { NotificationsNone, Language, Settings } from "@mui/icons-material";
+
+function TopNavBar(props) {
+  return (
+    <div className="topnavbar">
+      <div className="topnavbarWrapper">
+        <div className="topnavLeft">
+          <span className="mainlogo">anAdminDash</span>
+        </div>
+        <div className="topnavRight">
+          <div className="topnavbarIconContainer">
+            <Language />
+          </div>
+          <div className="topnavbarIconContainer">
+            <Settings />
+          </div>
+          <div className="topnavbarIconContainer">
+            <NotificationsNone />
+            <span className="topnavIconBadge">2</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TopNavBar;
